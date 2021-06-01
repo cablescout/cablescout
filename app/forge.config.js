e1e7196ab6dc82a7648c30ba7344a0732b974441
Zohar Zilberman <popen2@gmail.com>
2021-06-01 07:19:42 +0300

    app: Switch to electron-forge

---
+++ app/forge.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  packagerConfig: {
+    name: 'Cablescout',
+    out: './out',
+  },
+
+  makers: [
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin', 'win'],
+    },
+  ],
+
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'cablescout',
+          name: 'cablescout',
+        },
+        prerelease: true,
+      },
+    },
+  ],
+}
