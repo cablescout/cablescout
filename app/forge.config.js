f45287cc59d5177c9db2ef77ca8568b6a2be1a49
Zohar Zilberman <popen2@gmail.com>
2021-06-06 11:57:28 +0300

    app: Notarize

--- app/forge.config.js
+++ app/forge.config.js
@@ -1,14 +1,30 @@
 const process = require('process')
 
+const BUNDLE_ID = 'io.cablescout'
 const DAEMON_BASENAME = (process.platform === 'win32') ? 'cablescout-daemon.exe' : 'cablescout-daemon'
+const { APPLE_API_KEY, APPLE_API_ISSUER } = process.env
+
+const osxNotarize = (process.platform === 'darwin') ? {
+  appBundleId: BUNDLE_ID,
+  appleApiKey: APPLE_API_KEY,
+  appleApiIssuer: APPLE_API_ISSUER,
+} : undefined
 
 module.exports = {
   packagerConfig: {
     name: 'Cablescout',
+    appBundleId: BUNDLE_ID,
     out: './out',
     extraResource: [
       `../target/release/${DAEMON_BASENAME}`,
     ],
+    osxSign: {
+      "hardened-runtime": true,
+      "entitlements": "entitlements.plist",
+      "entitlements-inherit": "entitlements.plist",
+      "signature-flags": "library"
+    },
+    osxNotarize,
   },
 
   makers: [
